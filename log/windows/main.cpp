7d2ae29f0b3558419c6d88a1d8d0877bbf054ee0
sui <2076354958@qq.com>
2025-06-10 14:31:31 +0800

    添加Windows下日志

---
+++ log/windows/main.cpp
@@ -0,0 +1,18 @@
+﻿#include "log.hpp"
+#include <Windows.h>
+
+
+int main()
+{
+	DWORD bufferSize = MAX_PATH;
+	std::vector<TCHAR> buffer(bufferSize);
+
+	if (GetCurrentDirectory(bufferSize, buffer.data()) != 0) {
+		std::wcout << L"Current Working Directory: " << std::wstring(buffer.begin(), buffer.end()) << std::endl;
+	}
+
+	LOG(sui::My_level::debug) << "hello world" << "zz " << "tt";
+	Sleep(20000);
+	
+	return 0;
+}
